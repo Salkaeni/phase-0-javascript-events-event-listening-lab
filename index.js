8ce734e9874a3abefba9417fcc98a717b7a2031c
Salome <salomekaeni@gmail.com>
2024-07-25 02:26:40 -0700

    all

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+
+// index.js
+
+// Function to add event listener
 function addingEventListener() {
-}
+    // Grab the input element by its ID
+    const input = document.getElementById('button');
+  
+    // Define the click event handler
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the click event listener to the input element
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  
